624925c01726909a049c1d52c3a7d595fcb576f9
Manisha Choudhury <manisha03012002@gmail.com>
2024-03-29 10:56:16 +0530

    unique code modified

--- first/components/UniqueCode.js
+++ first/components/UniqueCode.js
@@ -1,15 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
 
 const UniqueCode = ({ navigation }) => {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState(['', '', '', '', '']);
+  const [isCodeComplete, setIsCodeComplete] = useState(false);
+  const inputRefs = useRef([...Array(5)].map(() => React.createRef()));
 
   const handleButtonClick = () => {
     // Add your logic to check the unique code and navigate to the login page
     // For now, let's just navigate to a placeholder screen named 'Login'
-    console.warn(code)
+    //console.warn(code)
+    const validCode = code.join('') === '5ROHY'; // Example validation logic
+    if (validCode) {
+      // Navigate to the next screen if the code is valid
+      console.warn(code) // Replace 'Login' with your actual screen name
+    } else {
+      // Code is not valid, handle accordingly (show error message, clear inputs, etc.)
+      console.warn('Invalid code. Please try again.');
+      // Clear inputs
+      setCode(['', '', '', '', '']);
+      // Set focus on the first input
+      inputRefs.current[0].focus();
+    }
   };
+  useEffect(() => {
+    const filledInputs = code.filter(input => input !== '').length;
+    setIsCodeComplete(filledInputs === 5);
+  }, [code]);
 
+  const handleInputChange = (text, index) => {
+    // Convert input text to uppercase
+    text = text.toUpperCase();
+
+    const newCode = [...code];
+    newCode[index] = text;
+    setCode(newCode);
+
+    if (text === '' && index > 0) {
+      // Move focus to the previous input if the current input is deleted
+      inputRefs.current[index -1].focus();
+    } else if (text === '' && index === 0) {
+      // Clear the current input and keep focus on it
+      inputRefs.current[index].clear();
+    } else if (text.length === 1 && index < 4) {
+      // Move focus to the next input
+      inputRefs.current[index + 1].focus();
+    }
+  };
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
@@ -17,37 +54,25 @@ const UniqueCode = ({ navigation }) => {
         <Text style={styles.subtitle}>Please enter your Unique Invite Code</Text>
 
         <View style={styles.codeContainer}>
-  {[...Array(5)].map((_, index) => (
-    <TextInput
-      key={index}
-      style={styles.codeInput}
-      maxLength={1}
-      keyboardType="ascii-capable"
-      onChangeText={(text) => {
-        if (text.length === 1 && index < 4) {
-          // Move focus to the next input
-          this[`inputRef${index + 1}`].focus();
-        }
-        // Update the code state
-        setCode((prevCode) => {
-          const newCode = [...prevCode];
-          newCode[index] = text;
-          return newCode;
-        });
-      }}
-      onKeyPress={({ nativeEvent: { key } }) => {
-        if (key === 'Backspace' && index > 0 && !code[index]-1) {
-          // Move focus to the previous input
-          this[`inputRef${index - 1}`].focus();
-        }
-      }}
-      ref={(input) => (this[`inputRef${index}`] = input)}
-    />
-  ))}
-</View>
+          {code.map((value, index) => (
+            <TextInput
+              key={index}
+              style={styles.codeInput}
+              maxLength={1}
+              autoCapitalize="characters" // Auto capitalize to uppercase
+              keyboardType="ascii-capable" // ASCII capable keyboard
+              value={value}
+              onChangeText={(text) => handleInputChange(text, index)}
+              ref={input => inputRefs.current[index] = input} // Correct ref assignment
+              //placeholder="-"
+              placeholderTextColor="grey" // Placeholder color
+            />
+          ))}
+        </View>
       </ScrollView>
+
       <View style={styles.buttonContainer}>
-        <Button title="Submit" onPress={handleButtonClick} />
+        <Button title="Submit" onPress={handleButtonClick} disabled={!isCodeComplete} />
       </View>
     </View>
   );
@@ -93,11 +118,10 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
     color: 'white',
   },
-  // buttonContainer: {
-  //   position: 'absolute',
-  //   bottom: 20, // Adjust bottom value as needed
-  //   alignSelf: 'center',
-  // },
+  buttonContainer: {
+    alignSelf: 'center',
+    marginTop: 20,
+  },
 });
 
 export default UniqueCode;
